a1281de4b85f7a7b351d9c56fd245d171903e1f4
Helvennn <enixgregory@gmail.com>
2025-02-17 01:07:42 +0700

    Variables Adjusted

--- Server/server.js
+++ Server/server.js
@@ -2,16 +2,17 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 const path = require('path');
+require('dotenv').config();
 
 const assignmentRoutes = require('./routes/assignments');
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT;
 
 app.use(cors());
 app.use(express.json());
 
-const MONGO_URI = 'mongodb://localhost:27017/kairos_db';
+const MONGO_URI = process.env.MONGO_URI;
 mongoose.connect(MONGO_URI)
 .then(() => console.log('Connected to MongoDB'))
 .catch((err) => console.error('MongoDB connection error: ', err));
