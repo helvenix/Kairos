405fdc5e8d32a6acd55e974ec84ccaafda0a30ba
Helvennn <enixgregory@gmail.com>
2025-02-16 21:42:03 +0700

    Assignment Database Model Added

---
+++ Server/models/Assignment.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose');
+
+const AssignmentSchema = new mongoose.Schema({
+    id: { type: Number, required: true, unique: true },
+    title: { type: String, required: true },
+    start: { type: Date, required: true },
+    deadline: { type: Date, required: true },
+    description: { type: String, default: '' },
+    notes: { type: String, default: '' },
+    tags: { type: [String], default: [] },
+    course: { type: String, default: '' },
+    attachments: { type: [String], default: [] },
+    submission: { type: String, default: '' },
+    timeRatio: { type: Number, default: 0 },
+    priority: { type: String, default: '' },
+    completed: { type: Boolean, default: false },
+    createdAt: { type: Date, default: Date.now },
+    completionDate: { type: Date }
+});
+
+module.exports = mongoose.model('Assignment', AssignmentSchema);
