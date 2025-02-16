fef6b796f888aabe72a86b78485305c89747bd0d
Helvennn <enixgregory@gmail.com>
2025-02-16 21:35:30 +0700

    Express server.js Setup

--- Server/server.js
+++ Server/server.js
@@ -1,4 +1,30 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
+const path = require('path');
 
+const assignmentRoutes = require('./routes/assignments');
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+const MONGO_URI = 'mongodb://localhost:27017/kairos_db';
+mongoose.connect(MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
+.then(() => console.log('Connected to MongoDB'))
+.catch((err) => console.error('MongoDB connection error: ', err));
+
+app.use('/api/assignments', assignmentRoutes);
+
+app.get('/', (req, res) => {
+    res.send('Hello from Kairos Express API!');
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
